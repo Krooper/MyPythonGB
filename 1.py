ec8795ecb54c9c977fe16baf633e593e469cc5fa
Krooper <98321262+Krooper@users.noreply.github.com>
2022-09-06 20:40:50 +0300

    made 1st task

--- 1.py
+++ 1.py
@@ -4,34 +4,36 @@
 # - 6 -> да
 # - 7 -> да
 # - 1 -> нет
-
-def is_number (number):
-    if number.isdigit():
-        return True
-    else:
-        print("Неправильный ввод!")
-        return False
-
-
-def in_range (number):
-    if 1 <= number <= 7:
-        return True
-    else:
-        print("Нет такого дня недели!")
+def day_of_week:
+    def is_number (number):
+        if number.isdigit():
+            return True
+        else:
+            print("Неправильный ввод!")
+            return False
 
 
-def day_off (number):
-    if 6 <= number <= 7:
-        print("Да")
-    else:
-        print("Нет")
+    def in_range (number):
+        if 1 <= number <= 7:
+            return True
+        else:
+            print("Нет такого дня недели!")
 
 
-input_number = input()
-if is_number(input_number):
-    week_day = int(input_number)
-    if in_range(week_day):
-        day_off(week_day)
+    def day_off (number):
+        if 6 <= number <= 7:
+            print("Да")
+        else:
+            print("Нет")
+
+
+    input_number = input()
+    if is_number(input_number):
+        week_day = int(input_number)
+        if in_range(week_day):
+            day_off(week_day)
+
+day_of_week()
 
 
 
