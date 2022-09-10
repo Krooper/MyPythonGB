f30935d9a0ef24a3e88defc7ba97d34ae5cc8a5f
Krooper <98321262+Krooper@users.noreply.github.com>
2022-09-10 12:58:47 +0300

    made 1st task

---
+++ 3.py
@@ -0,0 +1,46 @@
+def odd_num_sum():
+    def is_number(number):
+        try:
+            float(number)
+            return True
+        except ValueError:
+            print("Вы ввели не число!")
+            return False
+
+    def is_integer(number):
+        if number.isdigit():
+            return True
+        else:
+            print("Вы ввели дробное число!")
+            return False
+
+    def list_input():
+        num_list = []
+        print("Введите длину массива")
+        list_length = input()
+        while not is_number(list_length) or not is_integer(list_length):
+            print("Повторите ввод!")
+            list_length = input()
+        for i in range(int(list_length)):
+            print(f"Введите {i+1}-й элемент массива")
+            num_list.append(input())
+            while not is_number(num_list[i]):
+                print("Повторите ввод!")
+                num_list[i] = input()
+            num_list[i] = float(num_list[i])
+        return num_list
+
+    def odd_sum(numbers):
+        i = 1
+        num_sum = 0
+        while i < len(numbers):
+            num_sum += numbers[i]
+            i += 2
+        return num_sum
+
+    numbers_list = list_input()
+    print(f"Получившийся массив: {numbers_list}")
+    print(f"Сумма нечетных элементов: {odd_sum(numbers_list)}")
+
+
+odd_num_sum()
